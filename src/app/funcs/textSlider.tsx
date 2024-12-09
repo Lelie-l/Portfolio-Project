c155e5eca3bf4c8413683e7e168910daf2908fee
Kaiden <122194945+Lelie-l@users.noreply.github.com>
2024-12-09 21:28:30 +0700

    changes for deployment

--- src/app/funcs/textSlider.tsx
+++ src/app/funcs/textSlider.tsx
@@ -12,10 +12,10 @@ const TextSlider = () => {
                 <span>Success is not final, failure is not fatal.</span>
               </div>
               <div className="slidingTextRow">
-                <span>Believe you can and you're halfway there.</span>
-                <span>Believe you can and you're halfway there.</span>
-                <span>Believe you can and you're halfway there.</span>
-                <span>Believe you can and you're halfway there.</span>
+                <span>Believe you can and you&apos;re halfway there.</span>
+                <span>Believe you can and you&apos;re halfway there.</span>
+                <span>Believe you can and you&apos;re halfway there.</span>
+                <span>Believe you can and you&apos;re halfway there.</span>
               </div>
               <div className="slidingTextRow">
                 <span>The only limit to our realization of tomorrow is our doubts.</span>
@@ -24,16 +24,16 @@ const TextSlider = () => {
                 <span>The only limit to our realization of tomorrow is our doubts.</span>
               </div>
               <div className="slidingTextRow">
-                <span>It always seems impossible until it's done.</span>
-                <span>It always seems impossible until it's done.</span>
-                <span>It always seems impossible until it's done.</span>
-                <span>It always seems impossible until it's done.</span>
+                <span>It always seems impossible until it&apos;s done.</span>
+                <span>It always seems impossible until it&apos;s done.</span>
+                <span>It always seems impossible until it&apos;s done.</span>
+                <span>It always seems impossible until it&apos;s done.</span>
               </div>
               <div className="slidingTextRow">
-                <span>Don't watch the clock; do what it does.</span>
-                <span>Don't watch the clock; do what it does.</span>
-                <span>Don't watch the clock; do what it does.</span>
-                <span>Don't watch the clock; do what it does.</span>
+                <span>Don&apos;t watch the clock; do what it does.</span>
+                <span>Don&apos;t watch the clock; do what it does.</span>
+                <span>Don&apos;t watch the clock; do what it does.</span>
+                <span>Don&apos;t watch the clock; do what it does.</span>
               </div>
               <div className="slidingTextRow">
                 <span>The best way to predict the future is to create it.</span>
@@ -42,16 +42,16 @@ const TextSlider = () => {
                 <span>The best way to predict the future is to create it.</span>
               </div>
               <div className="slidingTextRow">
-                <span>You miss all of the shots you don't take.</span>
-                <span>You miss all of the shots you don't take.</span>
-                <span>You miss all of the shots you don't take.</span>
-                <span>You miss all of the shots you don't take.</span>
+                <span>You miss all of the shots you don&apos;t take.</span>
+                <span>You miss all of the shots you don&apos;t take.</span>
+                <span>You miss all of the shots you don&apos;t take.</span>
+                <span>You miss all of the shots you don&apos;t take.</span>
               </div>
               <div className="slidingTextRow">
-                <span>Opportunities don't happen, you create them.</span>
-                <span>Opportunities don't happen, you create them.</span>
-                <span>Opportunities don't happen, you create them.</span>
-                <span>Opportunities don't happen, you create them.</span>
+                <span>Opportunities don&apos;t happen, you create them.</span>
+                <span>Opportunities don&apos;t happen, you create them.</span>
+                <span>Opportunities don&apos;t happen, you create them.</span>
+                <span>Opportunities don&apos;t happen, you create them.</span>
               </div>
               <div className="slidingTextRow">
                 <span>Success is not final, failure is not fatal.</span>
@@ -60,10 +60,10 @@ const TextSlider = () => {
                 <span>Success is not final, failure is not fatal.</span>
               </div>
               <div className="slidingTextRow">
-                <span>Believe you can and you're halfway there.</span>
-                <span>Believe you can and you're halfway there.</span>
-                <span>Believe you can and you're halfway there.</span>
-                <span>Believe you can and you're halfway there.</span>
+                <span>Believe you can and you&apos;re halfway there.</span>
+                <span>Believe you can and you&apos;re halfway there.</span>
+                <span>Believe you can and you&apos;re halfway there.</span>
+                <span>Believe you can and you&apos;re halfway there.</span>
               </div>
               <div className="slidingTextRow">
                 <span>The only limit to our realization of tomorrow is our doubts.</span>
@@ -72,16 +72,16 @@ const TextSlider = () => {
                 <span>The only limit to our realization of tomorrow is our doubts.</span>
               </div>
               <div className="slidingTextRow">
-                <span>It always seems impossible until it's done.</span>
-                <span>It always seems impossible until it's done.</span>
-                <span>It always seems impossible until it's done.</span>
-                <span>It always seems impossible until it's done.</span>
+                <span>It always seems impossible until it&apos;s done.</span>
+                <span>It always seems impossible until it&apos;s done.</span>
+                <span>It always seems impossible until it&apos;s done.</span>
+                <span>It always seems impossible until it&apos;s done.</span>
               </div>
               <div className="slidingTextRow">
-                <span>Don't watch the clock; do what it does.</span>
-                <span>Don't watch the clock; do what it does.</span>
-                <span>Don't watch the clock; do what it does.</span>
-                <span>Don't watch the clock; do what it does.</span>
+                <span>Don&apos;t watch the clock; do what it does.</span>
+                <span>Don&apos;t watch the clock; do what it does.</span>
+                <span>Don&apos;t watch the clock; do what it does.</span>
+                <span>Don&apos;t watch the clock; do what it does.</span>
               </div>
               <div className="slidingTextRow">
                 <span>The best way to predict the future is to create it.</span>
@@ -90,16 +90,16 @@ const TextSlider = () => {
                 <span>The best way to predict the future is to create it.</span>
               </div>
               <div className="slidingTextRow">
-                <span>You miss all of the shots you don't take.</span>
-                <span>You miss all of the shots you don't take.</span>
-                <span>You miss all of the shots you don't take.</span>
-                <span>You miss all of the shots you don't take.</span>
+                <span>You miss all of the shots you don&apos;t take.</span>
+                <span>You miss all of the shots you don&apos;t take.</span>
+                <span>You miss all of the shots you don&apos;t take.</span>
+                <span>You miss all of the shots you don&apos;t take.</span>
               </div>
               <div className="slidingTextRow">
-                <span>Opportunities don't happen, you create them.</span>
-                <span>Opportunities don't happen, you create them.</span>
-                <span>Opportunities don't happen, you create them.</span>
-                <span>Opportunities don't happen, you create them.</span>
+                <span>Opportunities don&apos;t happen, you create them.</span>
+                <span>Opportunities don&apos;t happen, you create them.</span>
+                <span>Opportunities don&apos;t happen, you create them.</span>
+                <span>Opportunities don&apos;t happen, you create them.</span>
               </div>
             </div>
 
